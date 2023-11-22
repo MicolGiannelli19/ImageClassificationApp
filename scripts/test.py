8715ef141639a83ca1683bf4fa630283b8469f6d
MicolGiannelli19 <micol.giannelli@outlook.com>
2023-11-22 15:05:44 +0000

    desinging the structure of the app

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,11 @@
+from matplotlib import pyplot as plt
+from tensorflow.keras import models, layers, datasets
+import os
+import random
 import tensorflow as tf
 
-print("Hello World")
+# this is so that the code under this if statment is not run when we import something from this script
+if __name__ == "__main__":
+    # create output directoru if it doesn't already exist
+    if not os.path.exists("output"):
+        os.mkdir("output")
