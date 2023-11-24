b5ba0a86d138ed744e153f1a85061c15c8ab3a37
MicolGiannelli19 <micol.giannelli@outlook.com>
2023-11-24 16:52:41 +0000

    adding front end readme

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>What is this code tab for </code> ciao ciao ciao
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
